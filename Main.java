c361c1612ec14882cfc5aeef55953f5fbf7c4296
Kevin Day <kevinday.home@gmail.com>
2016-12-04 00:43:51 -0600

    User Input for search type, start and goal states

--- Main.java
+++ Main.java
@@ -1,28 +1,101 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class Main
 {
     public static void main(String [] args)
     {
-        String goalState = "Stormwind";
+        Scanner input = new Scanner(System.in);
 
-        MapTimeEvaluationFunction ef = new MapTimeEvaluationFunction(goalState);
+        InputReader inputReader = new InputReader("Input.txt");
+
+        int tempNum = 0;
+        for(String city : (inputReader.getCities()))
+        {
+            System.out.println(tempNum + ") " + city);
+            tempNum++;
+        }
+        System.out.print("Choose a starting city: ");
+        int startingCity = input.nextInt();
+
+        System.out.print("Choose a destination city: ");
+        int destinationCity = input.nextInt();
+
+        System.out.println("0) Find path by distance.");
+        System.out.println("1) Find path by time traveled.");
+        int searchChoice = input.nextInt();
+
+        String startState = inputReader.getCities().get(startingCity);
+        String goalState = inputReader.getCities().get(destinationCity);
+
+        EvaluationFunction ef;
+        StepCostFunction stepCostFunction;
+
+        if (searchChoice == 0)
+        {
+            ef = new MapDistanceEvaluationFunction(goalState);
+            stepCostFunction = new MapDistanceStepCostFunction();
+        }
+        else if (searchChoice == 1)
+        {
+            ef = new MapTimeEvaluationFunction(goalState);
+            stepCostFunction = new MapTimeStepCostFunction();
+        }
+        else
+        {
+            ef = new MapDistanceEvaluationFunction(goalState);
+            stepCostFunction = new MapDistanceStepCostFunction();
+        }
+
         MapMoveActionsFunction actionsFunction = new MapMoveActionsFunction();
         MapResultFunction resultFunction = new MapResultFunction();
         MapGoalTest goalTest = new MapGoalTest(goalState);
-        MapTimeStepCostFunction stepCostFunction = new MapTimeStepCostFunction();
 
-        Problem problem = new Problem("Silvermoon", actionsFunction, resultFunction, goalTest, stepCostFunction);
+        Problem problem = new Problem(startState, actionsFunction, resultFunction, goalTest, stepCostFunction);
 
         RecursiveBestFirstSearch rbfs = new RecursiveBestFirstSearch(ef);
 
         List<Action> result = rbfs.search(problem);
 
-        System.out.print("Silvermoon");
-        for(Action a : result)
+        double totalTravel = 0;
+        if (searchChoice == 0)
         {
-            System.out.print(" -> " + ((MapMoveAction)a).getDestination());
+            String prev = startState;
+            System.out.print(startState);
+            for(Action a : result)
+            {
+                String current = ((MapMoveAction)a).getDestination();
+                int prevIndex = inputReader.getCities().indexOf(prev);
+                int currentIndex = inputReader.getCities().indexOf(current);
+
+                double distance = inputReader.getDistances().get(prevIndex).get(currentIndex);
+                totalTravel = totalTravel + distance;
+
+                System.out.printf(" --(%.2f)--> %s", distance, current);
+                prev = current;
+            }
+            System.out.println();
+            System.out.printf("Total Distance Traveled: %.2f\n\n", totalTravel);
         }
-        System.out.println();
+        else if (searchChoice == 1)
+        {
+            String prev = startState;
+            System.out.print(startState);
+            for(Action a : result)
+            {
+                String current = ((MapMoveAction)a).getDestination();
+                int prevIndex = inputReader.getCities().indexOf(prev);
+                int currentIndex = inputReader.getCities().indexOf(current);
+                double distanceTraveled = inputReader.getDistances().get(prevIndex).get(currentIndex);
+                double speedLimit = inputReader.getSpeeds().get(prevIndex).get(currentIndex);
+                double timeTraveled = (distanceTraveled/speedLimit) * 60;
+                totalTravel = totalTravel + timeTraveled;
+
+                System.out.printf(" --(%.2f)--> %s", timeTraveled, current);
+                prev = current;
+            }
+            System.out.println();
+            System.out.printf("Total Travel Time: %.2f\n\n", totalTravel);
+        }
     }
 }
